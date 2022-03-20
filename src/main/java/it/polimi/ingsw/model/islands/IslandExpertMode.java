482991e81ac315e76476e0a7fbe243597762d351
Federico <federico.zanon@mail.polimi.it>
2022-03-20 15:45:20 +0100

    newIslandGroup method implemented

--- src/main/java/it/polimi/ingsw/model/islands/IslandExpertMode.java
+++ src/main/java/it/polimi/ingsw/model/islands/IslandExpertMode.java
@@ -6,19 +6,46 @@ import it.polimi.ingsw.model.enums.ColorS;
 public class IslandExpertMode extends Island{
     private boolean entryTile;
     private boolean countTowers;
-    private boolean additionalInfluence;
+    private boolean additionalInfluence;            //in realtà sarebbe meglio mettere l'attributo sul playerExpertMode
     private ColorS noInfluenceColor;
 
     public IslandExpertMode(int id){
         super(id);
         this.entryTile=false;
-        this.countTowers=false;
+        this.countTowers=true;
         this.additionalInfluence=false;
         this.noInfluenceColor=null;
     }
 
-    @Override
-    public Player getSupremacy(){
+    public ColorS getNoInfluenceColor() {
+        return noInfluenceColor;
+    }
 
+    public boolean isEntryTile(){
+        return entryTile;
+    }
+
+    public boolean isAdditionalInfluence() {
+        return additionalInfluence;
+    }
+
+    public boolean isCountTowers() {
+        return countTowers;
+    }
+
+    public void setAdditionalInfluence(boolean additionalInfluence) {
+        this.additionalInfluence = additionalInfluence;
+    }
+
+    public void setCountTowers(boolean countTowers) {
+        this.countTowers = countTowers;
+    }
+
+    public void setEntryTile(boolean entryTile) {
+        this.entryTile = entryTile;
+    }
+
+    public void setNoInfluenceColor(ColorS noInfluenceColor) {
+        this.noInfluenceColor = noInfluenceColor;
     }
 }
