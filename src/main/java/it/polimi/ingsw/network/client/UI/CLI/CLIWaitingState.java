35fcd7304c39c884b23d552b82b3f878993b7266
Federico <federico.zanon@mail.polimi.it>
2022-05-25 18:52:50 +0200

    fixed username case sensitive problems

--- src/main/java/it/polimi/ingsw/network/client/UI/CLI/CLIWaitingState.java
+++ src/main/java/it/polimi/ingsw/network/client/UI/CLI/CLIWaitingState.java
@@ -3,19 +3,31 @@ package it.polimi.ingsw.network.client.UI.CLI;
 import it.polimi.ingsw.network.client.Client;
 import it.polimi.ingsw.network.client.states.AbstractClientState;
 
-
+/**
+ * CLI waiting state class
+ */
 public class CLIWaitingState extends AbstractClientState {
-    private final Client client;
 
+    /**
+     * builds a CLI waiting state class
+     * @param client client
+     */
     public CLIWaitingState(Client client){
         this.client = client;
     }
 
+    /**
+     * displays waiting state on command line
+     */
     @Override
     public void render(){
         System.out.println(client.getWaitingMessage());
     }
 
+    /**
+     * manages server error on command line
+     * @param message error message
+     */
     @Override
     public void serverError(String message) {
         //FIXME: it's only an ester egg
--- src/main/java/it/polimi/ingsw/network/client/states/AbstractClientState.java
+++ src/main/java/it/polimi/ingsw/network/client/states/AbstractClientState.java
@@ -1,13 +1,31 @@
 package it.polimi.ingsw.network.client.states;
 
+import it.polimi.ingsw.network.client.Client;
+
+/**
+ * abstract client state class, to be extended by CLI or GUI implementation
+ */
 public abstract class AbstractClientState {
 
+    protected Client client;
+
+    /**
+     * displays the client state
+     */
     public abstract void render();
 
+    /**
+     * displays and manages an error sent by the server
+     * @param message error message
+     */
     public void serverError(String message){
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * displays a disconnection error
+     * @param message error message
+     */
     public void disconnectionError(String message){
         System.out.println(message);
     }
