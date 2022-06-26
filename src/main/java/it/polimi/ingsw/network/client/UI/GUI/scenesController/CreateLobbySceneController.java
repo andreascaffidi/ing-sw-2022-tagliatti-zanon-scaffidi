cda13a1a204bf80a33d457a0f3fef2985d07b145
andrea.scaffidi <andrea.scaffidi@inter.it>
2022-06-26 22:46:11 +0200

    Cli and Gui UX and Ui improved

--- src/main/java/it/polimi/ingsw/network/client/UI/GUI/scenesController/CreateLobbySceneController.java
+++ src/main/java/it/polimi/ingsw/network/client/UI/GUI/scenesController/CreateLobbySceneController.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.network.client.UI.GUI.scenesController;
 
 import it.polimi.ingsw.network.requests.setupMessages.CreateLobbyMessage;
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -14,6 +15,12 @@ public class CreateLobbySceneController extends AbstractSceneController {
     private Text alertMessage;
     @FXML
     private TextFlow alert;
+
+    @FXML
+    private ToggleGroup players;
+
+    @FXML
+    private ToggleGroup gameMode;
 
     private String mode = "normal";
     private int num = 2;
@@ -31,47 +38,9 @@ public class CreateLobbySceneController extends AbstractSceneController {
      * sends the lobby setting (dispatched by clicking enter button)
      */
     public void enter(){
+        this.num = Integer.parseInt(players.getSelectedToggle().getUserData().toString());
+        this.mode = gameMode.getSelectedToggle().getUserData().toString();
+        System.out.print(this.num+" "+ this.mode);
         client.send(new CreateLobbyMessage(client.getUsername(), this.mode.toUpperCase(), this.num));
     }
-
-    /**
-     * sets expert mode
-     */
-    public void expert()
-    {
-        this.mode = "expert";
-    }
-
-    /**
-     * sets normal mode
-     */
-    public void normal()
-    {
-        this.mode = "normal";
-    }
-
-    /**
-     * sets two players match
-     */
-    public void two()
-    {
-        this.num = 2;
-    }
-
-    /**
-     * sets three players match
-     */
-    public void tree()
-    {
-        this.num = 3;
-    }
-
-    /**
-     * sets four players match
-     */
-    public void four()
-    {
-        this.num = 4;
-    }
-
 }
